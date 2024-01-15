665294ac8f7559639d23ae45c1c431ea48814a75
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2024-01-16 02:00:49 +0600

    create pkg

---
+++ ros2_ws/build/my_robot-description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/emon/development/ros2-beginners-level-2/ros2_ws/src/my_robot-description
+# Build directory: /home/emon/development/ros2-beginners-level-2/ros2_ws/build/my_robot-description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
